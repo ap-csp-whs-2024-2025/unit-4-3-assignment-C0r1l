110c562594599b43baf0bfc15dcb247a74781300
C0r1l <carolygonzalez1458@gmail.com>
2024-12-17 22:45:53 +0000

    kawaii

--- calculator.cpp
+++ calculator.cpp
@@ -44,7 +44,35 @@ int main()
       {
         if (user_input == 2)
         {
-          
+          std::cout << "Enter two numbers \n";
+          std::cin >> num1 >> num2;
+          std::cout << "Their difference is " << (num1 - num2 ) << std::endl;
+        }
+        else
+        {
+          if (user_input == 3)
+          {
+            std::cout << "Enter two numbers \n";
+            std::cin >> num1 >> num2;
+            std::cout << "Their product is " << (num1 * num2) << std::endl;
+
+          }
+          else
+          {
+            if (user_input == 4)
+            {
+              std::cout << "Enter two numbers \n";
+              std::cin >> num1 >> num2;
+              std::cout << "Their quotient is " <<(num1 / num2) << std::endl;
+            }
+            else
+            {
+              if (user_input == 5)
+              std::cout << "Enter a number \n";
+              std::cin >> num1 ;
+              std::cout << "The square root is " << sqrt(num1) << std::endl;
+            }
+          }
         }
       }
     }
